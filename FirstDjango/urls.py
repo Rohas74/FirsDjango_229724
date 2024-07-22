6c2ed8bf60450b06af4a6cee30715ede64740bb7
Rohas74 <rohas74@yandex.ru>
2024-07-22 16:29:39 +0300

    126

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('about', views.home),
+    path('', views.home),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 def home(request):
     text = """
-    Имя: <b>Иван</b>
-    Отчество: <b>Петрович</b>
-    Фамилия: <b>Иванов</b>
-    телефон: <b>8-865-234-12-23</b>
-    e-mail: <b>vasia@mail.ru</b>
+    <p>Имя: <b>Иван</b></p>
+    <p>Отчество: <b>Петрович</b></p>
+    <p>Фамилия: <b>Иванов</b></hp>
+    <p>телефон: <b>8-865-234-12-23</b></p>
+    <hp>e-mail: <b>vasia@mail.ru</b></p>
     """
     return HttpResponse(text)
 # Create your views here.
