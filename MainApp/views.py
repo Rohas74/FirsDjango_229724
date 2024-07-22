1cfd1199aecd52c74dcc2f3c9eab44ad81ab9860
Rohas74 <rohas74@yandex.ru>
2024-07-22 15:52:12 +0300

    Вроде так

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from MainApp import views
 
+def home(request):
+    text = """
+    <h1>"Изучаем django<h1>
+    <strong>Автор</strong>: <i>Ivanov A.A.</i>
+    """
+    return HttpResponse(text)
 # Create your views here.
